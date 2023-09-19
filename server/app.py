e818ba449934a6e6cfe46b586eb6d9f5c30b9671
Eric <ericmuthuipatch22@gmail.com>
2023-09-19 19:16:00 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -17,11 +17,50 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants_serialized = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(
+            jsonify(plants_serialized),
+            200,
+        )
+
+        return response
+    
+    def post(self):
+        data = request.get_json()
+  
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'],
+        )
+        
+        db.session.add(new_plant)
+        db.session.commit()
+        
+        new_plant_dict = new_plant.to_dict()
+        
+        response = make_response(
+            jsonify(new_plant_dict),
+            201,
+        )
+
+        return response
+      
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant_dict = Plant.query.filter_by(id=id).first().to_dict()
+             
+        return make_response(
+            jsonify(plant_dict),
+            200,
+        )
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,3 +7,7 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    price = db.Column(db.Integer)
+
